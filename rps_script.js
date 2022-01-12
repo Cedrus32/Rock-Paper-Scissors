dd7169ded406226344c0a53ebe44190f1483bd59
Shannon <srosenb@protonmail.ch>
2022-01-12 15:30:05 -0600

    Add random int function for computer choice

--- rps_script.js
+++ rps_script.js
@@ -1,1 +1,11 @@
-// rock paper scissors script
+//rock == 0
+//paper == 1
+//scissors == 2
+
+
+//computer choice -- WORKING
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    return choice;
+}
+let computerChoice = computerPlay();
