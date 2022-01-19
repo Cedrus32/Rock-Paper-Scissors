cdaa71b6664ac4991c6ef0cf5355c19f4c1f5f9f
Shannon <srosenb@protonmail.ch>
2022-01-19 05:22:03 -0600

    Add basic hover interaction to 'play' button

---
+++ rps_splash.js
@@ -0,0 +1,17 @@
+function addHover(e) {
+    // console.log(e); // MOUSEOVER LOGS
+    btn.classList.add('add-hover');
+    // console.log(btn.classList); // HOUSEOVER ADDS .add-hover
+}
+
+function removeHover(e) {
+    // console.log(e); // MOUSELEAVE LOGS
+    btn.classList.remove('add-hover');
+    // console.log(btn.classList); // MOUSELEAVE REMOVES .add-hover
+}
+
+const btn = document.querySelector('button');
+// console.log(btn); // BTN LOGS
+console.log(btn.classList) // START classList IS ACCURATE
+btn.addEventListener('mouseover', addHover);
+btn.addEventListener('mouseleave', removeHover); // END classList IS ACCURATE
