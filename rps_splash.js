a4450342616492f40542cf8cee42898e0b6d6c88
Shannon <srosenb@protonmail.ch>
2022-01-19 17:56:51 -0600

    Include future code (commented out) for click functionality

--- rps_splash.js
+++ rps_splash.js
@@ -10,8 +10,16 @@ function removeHover(e) {
     // console.log(btn.classList); // MOUSELEAVE REMOVES .add-hover
 }
 
+// function addClick(e) {
+//     console.log(e)
+//     btn.classList.remove('add-hover');
+//     btn.classList.add('add-click');
+//     console.log(btn.classList);
+// }
+
 const btn = document.querySelector('button');
 // console.log(btn); // BTN LOGS
 // console.log(btn.classList) // START classList IS ACCURATE
 btn.addEventListener('mouseover', addHover);
 btn.addEventListener('mouseleave', removeHover);
+// btn.addEventListener('click', addClick);
