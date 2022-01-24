be4961cec4e0c2c482e908f6732c7d88cc21c453
Shannon <srosenb@protonmail.ch>
2022-01-24 06:40:05 -0600

    Add source list to thank-you button

--- index.js
+++ index.js
@@ -29,27 +29,8 @@ btn.addEventListener('mouseover', addHover);
 btn.addEventListener('mouseleave', removeHover);
 playLink.addEventListener('click', addClick);
 
-// ----------------- //
-// SOURCES ANIMATION //
-// ----------------- //
+// ------------------- //
+// THANK YOU ANIMATION //
+// ------------------- //
 
-const thankYou = document.querySelector('div.thank-you');
-let links = ['',
-        '<a href="">Cat Paw Images</a>',
-        '',
-        '<a href="">Clear Line Font</a>',
-        '<a href="">KG Neatly Printed Font</a>',
-        '',
-        '<a href="">Terra-Cotta Instagram Highlight Colors</a>']
-function genSources(e) {
-    // console.log(e);
-    const srcList = document.createElement('div');
-    const srcUL = document.createElement('ul');
-    const srcLI = document.createElement('li');
-    srcUL.textContent = ''
-    thankYou.appendChild(srcList);
-    srcList.textContent = 'source list';
-}
 
-const srcLink = document.querySelector('p.src-link');
-srcLink.addEventListener('click', genSources);
